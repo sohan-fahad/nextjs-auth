ab6b864c6625eaca2e0ec34c6821a0477008f413
sohan-fahad <sohanfahad@gmail.com>
2023-08-29 18:38:12 +0600

    update db url

--- src/app/api/sse/connect/route.ts
+++ src/app/api/sse/connect/route.ts
@@ -1,14 +1,17 @@
 import { SSE_EVENTS } from "@/data/constants/sse-event.constants";
 import { ISseEventPayload, responseStream, sendEvent } from "@/lib/events";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    sendEvent({ type: SSE_EVENTS.CONNECTION_STABLISHED, payload: {} })
+    // sendEvent({ type: SSE_EVENTS.CONNECTION_STABLISHED, payload: {} })
 
-    return new Response(responseStream.readable, {
-        headers: {
-            'Content-Type': 'text/event-stream',
-            Connection: 'keep-alive',
-            'Cache-Control': 'no-cache, no-transform',
-        },
-    });
+    // return new Response(responseStream.readable, {
+    //     headers: {
+    //         'Content-Type': 'text/event-stream',
+    //         Connection: 'keep-alive',
+    //         'Cache-Control': 'no-cache, no-transform',
+    //     },
+    // });
+
+    return NextResponse.json({ message: "error.meassage", success: false, statusCode: 400 }, { status: 400 });
 }
